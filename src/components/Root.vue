04cdf22c18cfd08c1773359f5b8843edbbc28901
abdullaqudrat <aqudrat630@gmail.com>
2019-01-08 14:11:30 -0700

    Adds search result functionality
    Adds:
    -getRecommendations function that makes api call based on input in search bar
    -conditional rendering for rendering search resutls

--- src/components/Root.vue
+++ src/components/Root.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="Search">
     <h1>{{ msg }}</h1>
+    <div>
+      <input type="text" v-model="inputVal">
+    </div>
+    <div><button v-on:click="getRecommendations()">submit</button></div>
+    <section v-if="errored">
+    <p>We're sorry, we're not able to retrieve this information at the moment, please try back later</p>
+  </section>
+
+  <section v-else>
+    <div v-if="loading">Loading...</div>
+    <div 
+      v-else
+      v-for="result in info"
+    >
+      {{ result.Name }}:{{ result.Type }}:{{ result.wUrl }}:{{ result.yUrl }}
+    </div>
+  </section>
   </div>
 </template>
 
@@ -10,10 +27,28 @@ export default {
   name: 'Search',
   data () {
     return {
-      msg: 'Search For Media Recommendations Here'
+      msg: 'Search For Media Recommendations Here',
+      inputVal: '',
+      info: null,
+      loading: false,
+      errored: false,
     }
   },
-  methods: {}
+  methods: {
+    getRecommendations(){
+      let input = this.inputVal.replace(/ /g,"+")
+      axios
+        .get(`https://tastedive.com/api/similar?info=1&k=327710-MediaMot-GQ5SN6GL&q=${input}`)
+        .then(response => {
+          this.info = response.data.Similar.Results
+        })
+        .catch(error => {
+          console.log(error)
+          this.errored = true
+        })
+        .finally(() => this.loading = false)
+    }
+  }
 }
 </script>
 
